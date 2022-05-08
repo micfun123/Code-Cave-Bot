60f3eb2fd62a1970f72f4c2d37f6e87835988031
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-05-08 10:43:08 +0400

    suggest

--- cogs/weekly.py
+++ cogs/weekly.py
@@ -12,30 +12,13 @@ class Weekly(commands.Cog):
         self.client = client
 
    
-
-
-@tasks.loop(time=time(10,00))
-async def weekly_challenge(self):
-    '''runs every day at 1PM UTC'''
-    
-    # check if the day is monday
-    today = datetime.utcnow().isoweekday()
-    if today == 5:  # Monday == 7
+    @commands.slash_command()
+    async def suggest_challenge(self, ctx):
         channel = self.get_channel(967975366482362428)
-        allmes = []
-        async for message in channel.history(limit=200):
-            allmes.append(message)
-        randoms = random.choice(allmes)
-        chennel2 = self.get_channel(967763538431082527)
-        em = discord.Embed(title=f"weekly challenge",color=0x00ff00)
-        em.description = "Its your favorite time of the week again!\n"
-        em.add_field(name="Challenge :", value=randoms.content)
-        msg = await channel.fetch_message(randoms.id)
-        print(msg.content)
-        await chennel2.send(embed=em)
-        await chennel2.send(f'<@&959429849804595230>')
-        await msg.delete()
-
+        await channel.send(f"{ctx.author.mention} suggested a challenge!")
+        await channel.send(f"{ctx.message.content}")
+        await ctx.message.delete()
+        await ctx.respond("Thanks for the suggestion!")
 
         
 def setup(client):
